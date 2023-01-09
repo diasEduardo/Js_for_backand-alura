2fd5d378de33d5086e7be67b36d91ebf0fbf00e1
diasEduardo <eduardo_dududex@hotmail.com>
2023-01-09 10:38:09 -0300

    arrow Functions

--- js_basic/function.js
+++ js_basic/function.js
@@ -12,3 +12,30 @@ timeInit = getNanoSecTime();
 Math.trunc();
 timeFinish = getNanoSecTime();
 console.log((timeFinish-timeInit))
+
+
+function test(arg,arg2){
+    return arg2;
+}
+
+console.log(test('1'));
+
+const arrowTest = name =>`Meu nome é ${name}`;
+const arrowSum = (num1,num2) => {console.log(num1,num2); return num1+num2}
+arrowSum(1,2);
+
+
+const num = 15;
+
+if (num < 0 ) {
+    console.log('outro número');
+} else if (num < 11) {
+  console.log('número entre zero e dez');
+} else if (num < 21) {
+  console.log('número entre dez e vinte');
+} else if (num < 31) {
+  console.log('número entre vinte e trinta');
+} else {
+  console.log('outro número');
+}
+
